635a586c825ca4eb372f66ea4afe8d301abb84d2
Ludmila Maia <ludmilacmaia@gmail.com>
2022-07-29 10:55:26 -0300

    altera app

--- src/app.js
+++ src/app.js
@@ -1,28 +1,27 @@
+require("dotenv-safe").config();
 const express = require('express');
 const index = require('./routes/index.js');
 const profissionais = require('./routes/profissionaisRoutes.js');
 const mongoose = require('./database/mongooseConnect');
 const swaggerUi = require('swagger-ui-express');
 const swaggerFile = require('../swagger/swagger_output.json');
-
-
-require("dotenv").config()
-
+const cors = require('cors')
 const app = express()
 mongoose.connect()
 
 app.use(express.json())
-
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'
-    )
-    next()
-});
+app.use(cors())
+// app.use(function(req, res, next) {
+//     res.header('Access-Control-Allow-Origin', '*')
+//     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'
+//     )
+//     next()
+// });
 
 app.use('/', index);
 app.use('/profissionais', profissionais);
 app.use('/minha-rota-de-documentacao', swaggerUi.serve, swaggerUi.setup(swaggerFile));
+
 
 
 
